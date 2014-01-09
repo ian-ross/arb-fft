ee7907f439e30ea687550f4aed640b5dc80dc84f
Ian Ross <ian@skybluetrades.net>
2014-01-09 22:10:54 +0100

    Add straight-line codelets for input length 13

--- Numeric/FFT/Special.hs
+++ Numeric/FFT/Special.hs
@@ -21,7 +21,8 @@ specialBases = IM.fromList [ (2, special2)
                            , (3, special3)
                            , (5, special5)
                            , (7, special7)
-                           , (11, special11) ]
+                           , (11, special11)
+                           , (13, special13) ]
 
 -- | Length 2 hard-coded FFT.
 special2 :: Int -> MVCD s -> MVCD s -> ST s ()
@@ -221,6 +222,141 @@ special11 sign xsin xsout = do
   MV.unsafeWrite xsout 10 $ if sign == 1 then r10 else r1
 
 -- | Length 13 hard-coded FFT.
+kp875502302, kp520028571, kp575140729 :: Double
+kp600477271, kp300462606, kp516520780 :: Double
+kp968287244, kp503537032, kp251768516 :: Double
+kp581704778, kp859542535, kp083333333 :: Double
+kp957805992, kp522026385, kp853480001 :: Double
+kp769338817, kp612264650, kp038632954 :: Double
+kp302775637, kp514918778, kp686558370 :: Double
+kp226109445, kp301479260 :: Double
+--kp866025403, kp500000000 :: Double
+kp875502302 = 0.875502302409147941146295545768755143177842006
+kp520028571 = 0.520028571888864619117130500499232802493238139
+kp575140729 = 0.575140729474003121368385547455453388461001608
+kp600477271 = 0.600477271932665282925769253334763009352012849
+kp300462606 = 0.300462606288665774426601772289207995520941381
+kp516520780 = 0.516520780623489722840901288569017135705033622
+kp968287244 = 0.968287244361984016049539446938120421179794516
+kp503537032 = 0.503537032863766627246873853868466977093348562
+kp251768516 = 0.251768516431883313623436926934233488546674281
+kp581704778 = 0.581704778510515730456870384989698884939833902
+kp859542535 = 0.859542535098774820163672132761689612766401925
+kp083333333 = 0.083333333333333333333333333333333333333333333
+kp957805992 = 0.957805992594665126462521754605754580515587217
+kp522026385 = 0.522026385161275033714027226654165028300441940
+kp853480001 = 0.853480001859823990758994934970528322872359049
+kp769338817 = 0.769338817572980603471413688209101117038278899
+kp612264650 = 0.612264650376756543746494474777125408779395514
+kp038632954 = 0.038632954644348171955506895830342264440241080
+kp302775637 = 0.302775637731994646559610633735247973125648287
+kp514918778 = 0.514918778086315755491789696138117261566051239
+kp686558370 = 0.686558370781754340655719594850823015421401653
+kp226109445 = 0.226109445035782405468510155372505010481906348
+kp301479260 = 0.301479260047709873958013540496673347309208464
+--kp866025403 = 0.866025403784438646763723170752936183471402627
+--kp500000000 = 0.500000000000000000000000000000000000000000000
+special13 :: Int -> MVCD s -> MVCD s -> ST s ()
+special13 sign xsin xsout = do
+  xr0 :+ xi0 <- MV.unsafeRead xsin 0 ; xr1 :+ xi1 <- MV.unsafeRead xsin 1
+  xr2 :+ xi2 <- MV.unsafeRead xsin 2 ; xr3 :+ xi3 <- MV.unsafeRead xsin 3
+  xr4 :+ xi4 <- MV.unsafeRead xsin 4 ; xr5 :+ xi5 <- MV.unsafeRead xsin 5
+  xr6 :+ xi6 <- MV.unsafeRead xsin 6 ; xr7 :+ xi7 <- MV.unsafeRead xsin 7
+  xr8 :+ xi8 <- MV.unsafeRead xsin 8 ; xr9 :+ xi9 <- MV.unsafeRead xsin 9
+  xr10 :+ xi10 <- MV.unsafeRead xsin 10 ; xr11 :+ xi11 <- MV.unsafeRead xsin 11
+  xr12 :+ xi12 <- MV.unsafeRead xsin 12
+  let t2d = xr8 - xr5 ; tf = xr8 + xr5 ; ta = xr10 + xr4 ; tq = xr10 - xr4
+      ty = kp500000000 * ta - xr12
+      tb = xr12 + ta ; tr = xr9 - xr3 ; t5 = xr3 + xr9 ; t6 = xr1 + t5
+      tx = xr1 - kp500000000 * t5
+      ti = xr11 + xr6 ; tt = xr11 - xr6 ; tu = xr7 - xr2 ; tl = xr7 + xr2
+      tc = t6 + tb ; t2n = t6 - tb ; t2b = ti - tl ; tm = ti + tl
+      t2e = tt + tu ; tv = tt - tu ; ts = tq - tr ; t2g = tr + tq
+      tz = tx - ty ; t2a = tx + ty
+      tA = tf - kp500000000 * tm
+      tn = tf + tm
+      t2f = t2d - kp500000000 * t2e
+      t2o = t2d + t2e ; to = tc + tn ; tH = tc - tn
+      t2h = t2f + kp866025403 * t2g ; t2k = t2f - kp866025403 * t2g
+      tE = tz - tA ; tB = tz + tA ; tF = ts - tv ; tw = ts + tv
+      t2j = t2a - kp866025403 * t2b ; t2c = t2a + kp866025403 * t2b
+      t1R = xi8 + xi5 ; tM = xi8 - xi5 ; t17 = xi10 + xi4 ; t10 = xi10 - xi4
+      t18 = kp500000000 * t17 - xi12
+      t1l = xi12 + t17 ; tX = xi9 - xi3 ; t14 = xi3 + xi9 ; t1k = xi1 + t14
+      t15 = xi1 - kp500000000 * t14
+      tP = xi11 - xi6 ; t1a = xi11 + xi6 ; t1b = xi7 + xi2 ; tS = xi7 - xi2
+      t1Q = t1k + t1l ; t1m = t1k - t1l ; t11 = tX + t10 ; t1W = t10 - tX
+      t1X = tP - tS ; tT = tP + tS ; t1S = t1a + t1b ; t1c = t1a - t1b
+      t19 = t15 + t18 ; t1Z = t15 - t18 ; t1j = tM + tT
+      tU = tM - kp500000000 * tT
+      t1T = t1R + t1S
+      t20 = t1R - kp500000000 * t1S ; t12 = tU + kp866025403 * t11
+      t1f = tU - kp866025403 * t11
+      t21 = t1Z + t20 ; t24 = t1Z - t20 ; t27 = t1Q - t1T ; t1U = t1Q + t1T
+      t1g = t19 - kp866025403 * t1c ; t1d = t19 + kp866025403 * t1c
+      t25 = t1W - t1X ; t1Y = t1W + t1X
+      tC = tw + kp301479260 * tB ; t1x = tB - kp226109445 * tw
+      t1y = tF + kp686558370 * tE ; tG = tE - kp514918778 * tF
+      t1n = t1j - kp302775637 * t1m ; t1G = t1m + kp302775637 * t1j
+      t1u = t1d - kp038632954 * t12 ; t1e = t12 + kp038632954 * t1d
+      t1h = t1f + kp612264650 * t1g ; t1v = t1g - kp612264650 * t1f
+      t1J = t1x + kp769338817 * t1y ; t1z = t1x - kp769338817 * t1y
+      t1H = t1u - kp853480001 * t1v ; t1w = t1u + kp853480001 * t1v
+      t1I = t1G - kp522026385 * t1H ; t1O = t1H + kp957805992 * t1G
+      tp = xr0 - kp083333333 * to ; t1E = t1e + kp853480001 * t1h
+      t1i = t1e - kp853480001 * t1h ; t1q = tH - kp859542535 * tG
+      tI = tG + kp581704778 * tH ; t1o = t1i + kp957805992 * t1n
+      t1s = t1n - kp522026385 * t1i ; t1p = tp - kp251768516 * tC
+      tD = tp + kp503537032 * tC ; t1C = t1w - kp968287244 * t1z
+      t1A = t1w + kp968287244 * t1z ; tJ = tD + kp516520780 * tI
+      t1N = tD - kp516520780 * tI ; t1D = t1p - kp300462606 * t1q
+      t1r = t1p + kp300462606 * t1q ; t1t = t1r - kp575140729 * t1s
+      t1B = t1r + kp575140729 * t1s ; t1L = t1D - kp520028571 * t1E
+      t1F = t1D + kp520028571 * t1E ; t1K = t1I + kp875502302 * t1J
+      t1M = t1I - kp875502302 * t1J ; t2D = t21 - kp226109445 * t1Y
+      t22 = t1Y + kp301479260 * t21 ; t26 = t24 - kp514918778 * t25
+      t2E = t25 + kp686558370 * t24 ; t2v = t2o - kp302775637 * t2n
+      t2p = t2n + kp302775637 * t2o ; t2i = t2c - kp038632954 * t2h
+      t2s = t2h + kp038632954 * t2c ; t2t = t2k + kp612264650 * t2j
+      t2l = t2j - kp612264650 * t2k ; t2F = t2D - kp769338817 * t2E
+      t2N = t2D + kp769338817 * t2E ; t2K = t2s + kp853480001 * t2t
+      t2u = t2s - kp853480001 * t2t ; t2w = t2u + kp957805992 * t2v
+      t2A = t2v - kp522026385 * t2u ; t1V = xi0 - kp083333333 * t1U
+      t2m = t2i - kp853480001 * t2l ; t2C = t2i + kp853480001 * t2l
+      t28 = t26 + kp581704778 * t27 ; t2y = t27 - kp859542535 * t26
+      t2M = t2p - kp522026385 * t2m ; t2q = t2m + kp957805992 * t2p
+      t23 = t1V + kp503537032 * t22 ; t2x = t1V - kp251768516 * t22
+      t2O = t2M - kp875502302 * t2N ; t2Q = t2M + kp875502302 * t2N
+      t2r = t23 + kp516520780 * t28 ; t29 = t23 - kp516520780 * t28
+      t2z = t2x + kp300462606 * t2y ; t2J = t2x - kp300462606 * t2y
+      t2P = t2J + kp520028571 * t2K ; t2L = t2J - kp520028571 * t2K
+      t2B = t2z + kp575140729 * t2A ; t2H = t2z - kp575140729 * t2A
+      t2I = t2C + kp968287244 * t2F ; t2G = t2C - kp968287244 * t2F
+      r12 = (tJ - kp600477271 * t1o) :+ (t2r + kp600477271 * t2w)
+      r11 =  (t1F + kp575140729 * t1K) :+ (t2L - kp575140729 * t2O)
+      r10 = (t1t + kp520028571 * t1A) :+ (t2B - kp520028571 * t2G)
+      r9 = (t1B + kp520028571 * t1C) :+ (t2H - kp520028571 * t2I)
+      r8 = (t1N + kp600477271 * t1O) :+ (t29 - kp600477271 * t2q)
+      r7 = (t1L + kp575140729 * t1M) :+ (t2P - kp575140729 * t2Q)
+      r6 = (t1F - kp575140729 * t1K) :+ (t2L + kp575140729 * t2O)
+      r5 = (t1N - kp600477271 * t1O) :+ (t29 + kp600477271 * t2q)
+      r4 = (t1t - kp520028571 * t1A) :+ (t2B + kp520028571 * t2G)
+      r3 = (t1B - kp520028571 * t1C) :+ (t2H + kp520028571 * t2I)
+      r2 = (t1L - kp575140729 * t1M) :+ (t2P + kp575140729 * t2Q)
+      r1 = (tJ + kp600477271 * t1o) :+ (t2r - kp600477271 * t2w)
+  MV.unsafeWrite xsout 0 $ (xr0 + to) :+ (xi0 + t1U)
+  MV.unsafeWrite xsout 1 $ if sign == 1 then r1 else r12
+  MV.unsafeWrite xsout 2 $ if sign == 1 then r2 else r11
+  MV.unsafeWrite xsout 3 $ if sign == 1 then r3 else r10
+  MV.unsafeWrite xsout 4 $ if sign == 1 then r4 else r9
+  MV.unsafeWrite xsout 5 $ if sign == 1 then r5 else r8
+  MV.unsafeWrite xsout 6 $ if sign == 1 then r6 else r7
+  MV.unsafeWrite xsout 7 $ if sign == 1 then r7 else r6
+  MV.unsafeWrite xsout 8 $ if sign == 1 then r8 else r5
+  MV.unsafeWrite xsout 9 $ if sign == 1 then r9 else r4
+  MV.unsafeWrite xsout 10 $ if sign == 1 then r10 else r3
+  MV.unsafeWrite xsout 11 $ if sign == 1 then r11 else r2
+  MV.unsafeWrite xsout 12 $ if sign == 1 then r12 else r1
 
 
 -- | Length 4 hard-coded FFT.
