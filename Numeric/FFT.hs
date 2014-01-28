472cf70004b97ca0f1b09672b8a81015bd7643af
Ian Ross <ian@skybluetrades.net>
2014-01-28 19:37:04 +0100

    Switch to generic vector interface for front-end API

--- Numeric/FFT.hs
+++ Numeric/FFT.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- | Mixed-radix FFT calculation.
 --
 -- Arbitrary input vector lengths are handled using a mixed-radix
@@ -11,7 +12,7 @@ module Numeric.FFT
        ) where
 
 import Prelude hiding (length, map, sum, zipWith)
-import Data.Vector
+import Data.Vector.Generic
 import Data.Complex
 
 import Numeric.FFT.Types
@@ -20,21 +21,25 @@ import Numeric.FFT.Execute
 
 
 -- | Forward FFT with embedded plan calculation.
-fft :: Vector (Complex Double) -> IO (Vector (Complex Double))
+fft :: Vector v (Complex Double) =>
+       v (Complex Double) -> IO (v (Complex Double))
 fft xs = do
   p <- plan $ length xs
   return $ fftWith p xs
 
 -- | Inverse FFT with embedded plan calculation.
-ifft :: Vector (Complex Double) -> IO (Vector (Complex Double))
+ifft :: Vector v (Complex Double) =>
+        v (Complex Double) -> IO (v (Complex Double))
 ifft xs = do
   p <- plan $ length xs
   return $ ifftWith p xs
 
 -- | Forward FFT with pre-computed plan.
-fftWith :: Plan -> Vector (Complex Double) -> Vector (Complex Double)
+fftWith :: Vector v (Complex Double) =>
+           Plan -> v (Complex Double) -> v (Complex Double)
 fftWith p = convert . execute p Forward . convert
 
 -- | Inverse FFT with pre-computed plan.
-ifftWith :: Plan -> Vector (Complex Double) -> Vector (Complex Double)
+ifftWith :: Vector v (Complex Double) =>
+            Plan -> v (Complex Double) -> v (Complex Double)
 ifftWith p = convert . execute p Inverse . convert
