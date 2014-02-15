9b71bc72820c7be9b41a90f307d53ffab19a777e
Ian Ross <ian@skybluetrades.net>
2014-02-15 18:04:19 +0100

    Fix #5: improve LLVM flag handling
    Make False default for LLVM flag.  Emit warning when building without
    LLVM, but do not attempt to locate LLVM executables, and do not
    override user's choice of flags.

--- Setup.hs
+++ Setup.hs
@@ -1,33 +1,25 @@
-import Control.Applicative ((<$>))
 import Control.Monad (when)
-import Data.List (delete)
-import Data.Maybe (isJust)
 import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.Configure
 import Distribution.Simple.LocalBuildInfo
-import Distribution.Simple.Program
 import Distribution.Simple.Setup
-import Distribution.Verbosity
 
 checkLLVM :: (GenericPackageDescription, HookedBuildInfo)
           -> ConfigFlags -> IO LocalBuildInfo
-checkLLVM (pkg, hbi) inflags = do
-  installed <- isJust <$> findProgramLocation verbose "llvm-config"
-  let llvmf = FlagName "llvm"
-      cfgflags = configConfigurationsFlags inflags
-      fixcfgflags = (llvmf, False) : delete (llvmf, True) cfgflags
-      fixflags = inflags { configConfigurationsFlags = fixcfgflags }
-  let (flags, msg) = case installed of
-        True -> (inflags, False)
-        False -> case lookup llvmf cfgflags of
-          Just True -> (fixflags, True)
-          Just False -> (inflags, False)
-          Nothing -> (fixflags, True)
-  when msg $ do
-    putStrLn "Warning: LLVM is not installed."
-    putStrLn "Building arb-fft with native code generator (slower)."
-  configure (pkg, hbi) flags
+checkLLVM (pkg, hbi) flgs = do
+  let warn = case lookup (FlagName "llvm") $ configConfigurationsFlags flgs of
+        Nothing    -> True
+        Just True  -> False
+        Just False -> True
+  when warn $ do
+    putStrLn "============================================================"
+    putStrLn " WARNING: LLVM flag is not set."
+    putStrLn ""
+    putStrLn " Building arb-fft with native code generator (slower)."
+    putStrLn " Reconfigure with -fllvm to build with LLVM."
+    putStrLn "============================================================"
+  configure (pkg, hbi) flgs
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks { confHook = checkLLVM }
